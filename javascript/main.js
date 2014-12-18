7d629d34a0dcae67f07282b917742aeabda0b35b
Jan Potoms <potoms.jan@gmail.com>
2014-12-19 00:20:51 +0100

    Add base url input

--- javascript/main.js
+++ javascript/main.js
@@ -5,16 +5,21 @@ var fs = require('fs');
 
 var $htmlTextArea = $('#to-parse-html');
 var $outputPane = $('#output');
+var $baseUrlInput = $('#base-url-input');
 
 var example = fs.readFileSync(__dirname + '/example.html', 'utf8');
 $htmlTextArea.val(example);
 
 function parseContent() {
   var toParse = $htmlTextArea.val();
+  var baseUrl = $baseUrlInput.val();
   var $ = cheerio.load(toParse);
-  var parsed = microdata.parse($);
+  var parsed = microdata.parse($, null, {
+    base: baseUrl
+  });
   $outputPane.text(JSON.stringify(parsed, null, 2));
 }
 
 $htmlTextArea.on('change', parseContent);
+$baseUrlInput.on('change', parseContent);
 parseContent();
