fe74a7447f1ae05e1eab3bc6d0bba0b1dbf2592e
Jan Potoms <potoms.jan@gmail.com>
2015-02-08 15:11:35 +0100

    Implement forgiving mode for top level itemprop on itemscope fixes https://github.com/Janpot/microdata-node/issues/2

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -2,22 +2,23 @@
 
 'use strict';
 
-var cheerio = require('cheerio'),
-    assert  = require('chai').assert,
+var assert  = require('chai').assert,
     parser  = require('..');
 
 describe('parser', function () {
 
   it('finds no data when none defined', function () {
-    var $ = cheerio.load('<div>hello</div>');
-    var result = parser.toJson($.html());
+    var html =
+      '<div>hello</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 0);
   });
 
   it.skip('finds an empty item', function () {
-    var $ = cheerio.load('<div itemscope>hello</div>');
-    var result = parser.toJson($.html());
+    var html =
+      '<div itemscope>hello</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.isObject(result.items[0]);
@@ -28,24 +29,28 @@ describe('parser', function () {
   });
 
   it('finds an item with a type', function () {
-    var $ = cheerio.load('<div itemscope itemtype="http://schema.org/Person">hello</div>');
-    var result = parser.toJson($.html());
+    var html =
+      '<div itemscope itemtype="http://schema.org/Person">hello</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].type, [ 'http://schema.org/Person' ]);
   });
 
   it.skip('finds an item with a global id', function () {
-    var $ = cheerio.load('<div itemscope itemid="  urn:isbn:0-330-34032-8 ">hello</div>');
-    var result = parser.toJson($.html());
+    var html =
+      '<div itemscope itemid="  urn:isbn:0-330-34032-8 ">hello</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].id, 'urn:isbn:0-330-34032-8');
   });
 
   it('finds an item with multiple types', function () {
-    var $ = cheerio.load('<div itemscope itemtype=" http://schema.org/Person  http://schema.org/PostalAddress  ">hello</div>');
-    var result = parser.toJson($.html());
+    var html = '<div itemscope itemtype=" http://schema.org/Person  http://schema.org/PostalAddress  ">' +
+    '  hello' +
+    '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].type, [
@@ -55,8 +60,11 @@ describe('parser', function () {
   });
 
   it('finds an item with type defined twice', function () {
-    var $ = cheerio.load('<div itemscope itemtype="http://schema.org/Person http://schema.org/Person">hello</div>');
-    var result = parser.toJson($.html());
+    var html =
+      '<div itemscope itemtype="http://schema.org/Person http://schema.org/Person">' +
+      '  hello' +
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].type, [
@@ -65,21 +73,23 @@ describe('parser', function () {
   });
 
   it('finds an item within an element', function () {
-    var $ = cheerio.load('<div><div itemscope itemtype="http://schema.org/Person">hello</div></div>');
-    var result = parser.toJson($.html());
+    var html =
+      '<div>' +
+      '  <div itemscope itemtype="http://schema.org/Person">hello</div>' +
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].type, [ 'http://schema.org/Person' ]);
   });
 
   it('finds multiple items within an element', function () {
-    var $ = cheerio.load(
+    var html = 
       '<div>' +
       '  <div itemscope itemtype="http://schema.org/Person">hello</div>' +
       '  <div itemscope itemtype="http://schema.org/PostalAddress">hello</div>' +
-      '</div>'
-    );
-    var result = parser.toJson($.html());
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 2);
     assert.deepEqual(result.items[0].type, [ 'http://schema.org/Person' ]);
@@ -87,13 +97,12 @@ describe('parser', function () {
   });
 
   it('finds an item with properties', function () {
-    var $ = cheerio.load(
+    var html = 
       '<div itemscope itemtype="http://schema.org/Person">' +
       '  <div itemprop="name">Jan</div>' +
       '  <div><div itemprop="age">29</div></div>' +
-      '</div>'
-    );
-    var result = parser.toJson($.html());
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].properties, {
@@ -103,7 +112,7 @@ describe('parser', function () {
   });
 
   it('finds an item with childitems', function () {
-    var $ = cheerio.load(
+    var html = 
       '<div itemscope itemtype="http://schema.org/Person">' +
       '  <div itemprop="address1" itemscope itemtype="http://schema.org/PostalAddress">' +
       '    <div itemprop="street">street1</div>' +
@@ -111,9 +120,8 @@ describe('parser', function () {
       '  <div itemprop="address2" itemscope itemtype="http://schema.org/PostalAddress">' +
       '    <div itemprop="street">street2</div>' +
       '  </div>' +
-      '</div>'
-    );
-    var result = parser.toJson($.html());
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
 
@@ -131,13 +139,12 @@ describe('parser', function () {
   });
 
   it('collates properties', function () {
-    var $ = cheerio.load(
+    var html = 
       '<div itemscope itemtype="http://schema.org/Person">' +
       '  <div itemprop="name">Jan</div>' +
       '  <div itemprop="name">Potoms</div>' +
-      '</div>'
-    );
-    var result = parser.toJson($.html());
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].properties, {
@@ -146,24 +153,22 @@ describe('parser', function () {
   });
 
   it('handles empty propertynames', function () {
-    var $ = cheerio.load(
+    var html = 
       '<div itemscope itemtype="http://schema.org/Person">' +
       '  <div itemprop="">Jan</div>' +
-      '</div>'
-    );
-    var result = parser.toJson($.html());
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].properties, {});
   });
 
   it('handles multiple propertynames', function () {
-    var $ = cheerio.load(
+    var html = 
       '<div itemscope itemtype="http://schema.org/Person">' +
       '  <div itemprop="name additionalName">Jan</div>' +
-      '</div>'
-    );
-    var result = parser.toJson($.html());
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].properties, {
@@ -173,12 +178,11 @@ describe('parser', function () {
   });
 
   it('handles duplicated propertynames', function () {
-    var $ = cheerio.load(
+    var html = 
       '<div itemscope itemtype="http://schema.org/Person">' +
       '  <div itemprop="  name  name ">Jan</div>' +
-      '</div>'
-    );
-    var result = parser.toJson($.html());
+      '</div>';
+    var result = parser.toJson(html);
     assert.isArray(result.items);
     assert.lengthOf(result.items, 1);
     assert.deepEqual(result.items[0].properties, {
@@ -186,20 +190,22 @@ describe('parser', function () {
     });
   });
 
-  it('only parses specified nodes', function () {
-    var $ = cheerio.load(
-      '<div itemscope></div>' +
-      '<div itemscope></div>' +
-      '<div class="this-one" itemscope>' +
+  it('finds top level items that are props in non-strict mode', function () {
+    var html = 
+      '<div itemscope itemprop="someprop">' +
       '  <div itemprop="name">Jan</div>' +
-      '</div>'
-    );
-    var result = parser.parse($, $('.this-one'));
-    assert.isArray(result.items);
-    assert.lengthOf(result.items, 1);
-    assert.deepEqual(result.items[0].properties, {
-      name: [ 'Jan' ]
-    });
+      '  <div><div itemprop="age">29</div></div>' +
+      '  <div itemprop="address" itemscope>' +
+      '    <span itemprop="street">my street</span>' +
+      '  </div>' +
+      '</div>';
+    var nonStrictResult = parser.toJson(html);
+    assert.isArray(nonStrictResult.items);
+    assert.lengthOf(nonStrictResult.items, 1);
+    assert.strictEqual(nonStrictResult.items[0].properties.name[0], 'Jan');
+    var strictResult = parser.toJson(html, { strict: true });
+    assert.isArray(strictResult.items);
+    assert.lengthOf(strictResult.items, 0);
   });
 
 });
